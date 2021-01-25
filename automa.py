537128c76d8813a55dc91ff57d7311053bab2015
Stuart Rankin <stuartrankin54@gmail.com>
2021-01-25 02:41:17 +0000

    added basic flags, save file and pefile functionality

--- automa.py
+++ automa.py
@@ -1,1 +1,43 @@
+import argparse
+import os
 import pefile
+
+
+#Flags
+parser = argparse.ArgumentParser()
+parser.add_argument("sample", help="Malware sample to be analysed")
+parser.add_argument("-o", "--output", type=argparse.FileType("w"), help="Specifies output file for results to be saved to. Defaults to output.html")
+args = parser.parse_args()
+
+
+def strings():
+    sampleStrings = os.system("strings " + args.sample)
+    print(sampleStrings)
+
+def saveResult():
+    
+    if args.output:
+        output_file = args.output
+
+    else:
+        output_file = open("output.html", "w") 
+    
+    output_file.write("test\n")
+    output_file.close()
+
+
+def peFile():
+    pe = pefile.PE(args.sample)
+#    help(pefile.PE)    
+    if pe.is_exe():
+        print("File is exe")
+    elif pe.is_dll():
+        print("File is dll")
+
+
+
+
+strings()
+peFile()
+saveResult()
+
