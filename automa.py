cb348d10deb738119468fa96181f19e4da22c4b3
Debian <debian@debian10.linuxvmimages.local>
2021-02-09 21:21:46 +0000

    added support for multiple files being passed

--- automa.py
+++ automa.py
@@ -1,6 +1,5 @@
 #Filesize needs added
 
-
 import argparse
 import os, subprocess
 import hashlib
@@ -12,7 +11,7 @@ api = "71f11256d158446b715bc3410886630f782ae6a10e151e2fb048b84f287b307d"
 
 #Flags
 parser = argparse.ArgumentParser()
-parser.add_argument("sample", help="Malware sample to be analysed")
+parser.add_argument("sample", help="Malware sample to be analysed", nargs='+')
 parser.add_argument("-o", "--output", type=argparse.FileType("w"), help="Specifies output file for results to be saved to. Defaults to output.html")
 args = parser.parse_args()
 
@@ -26,7 +25,7 @@ class Sample:
     malicious = False
 
     def strings(self):
-        os.system("python2.7 flosspractice.py " + args.sample + " > strings.txt")
+        os.system("python2.7 flosspractice.py " + self.name + " > strings.txt")
         f = open("strings.txt", "r")
         sampleStrings = f.read().splitlines()
         f.close()
@@ -42,24 +41,7 @@ class Sample:
 
         print(suspiciousStrings)
 
-    def saveResult(self, output):
-        if args.output:
-            output_file = args.output
     
-        else:
-            output_file = open("output.html", "w") 
-
-        output_file.write("Filename: " + filename)
-        output_file.write("MD5: " + md5)
-    
-
-        output_file.write("VirusTotal Results")
-        for key in results:
-            output_file.write(key + ": " + results[key]["category"])
-
-        output_file.close()
-
-
     def peFile(self):
         pe = pefile.PE(args.sample)
 #    help(pefile.PE)    
@@ -75,38 +57,40 @@ class Sample:
     def virustotal(self):
         client = vt.Client(api)
         try:
-            file = client.get_object("/files/" + md5)
+            file = client.get_object("/files/" + self.md5)
         except:
-            with open(filename, "rb") as f:
-                analysis = client.scan_file(args.sample, wait_for_completion=True)
-            file = client.get_object("/files/" + md5)
+            with open(self.name, "rb") as f:
+                analysis = client.scan_file(self.name, wait_for_completion=True)
+            file = client.get_object("/files/" + self.md5)
 
         client.close()
-        results = {}
-        malicious = []
-        undetected = []
+        self.results = {}
+        self.malicious = []
+        self.undetected = []
         for key in file.last_analysis_results:
-            results[key] = file.last_analysis_results[key]["category"]
-            if results[key] == "malicious":
-                malicious.append(key)
-            elif results[key] == "undetected":
-                undetected.append(key)
+            self.results[key] = file.last_analysis_results[key]["category"]
+            if self.results[key] == "malicious":
+                self.malicious.append(key)
+            elif self.results[key] == "undetected":
+                self.undetected.append(key)
         
-        return results, malicious, undetected
+       # results, malicious, undetected
         
     def format(self):
         output = ""
-        output += "<h1>" + sample.name + "</h1>\n"
-        output +="<table><tr><th>Filename</th><td>" + sample.name + "</td></tr>"
-        output += "<tr><th>MD5</th><td>" + sample.md5 + "</td></tr>"
+        output += "<h1>" + self.name + "</h1>\n"
+        output +="<table><tr><th>Filename</th><td>" + self.name + "</td></tr>"
+        output += "<tr><th>MD5</th><td>" + self.md5 + "</td></tr>"
         output += "</table>"
 
         output += "<h2>VirusTotal Results</h2>"
         output += "<table>"
-        for key in results:
-            output += "<tr><th>" + key + "</th><td>" + results[key] + "</td></tr>"
+        for key in self.results:
+            output += "<tr><th>" + key + "</th><td>" + self.results[key] + "</td></tr>"
+
 
         output += "</table>"
+        output += "The detection rate is: " + str(len(self.malicious)) + "/" + str(len(self.undetected) + len(self.malicious))
         return output
 
 #def analysis():
@@ -122,28 +106,27 @@ class Sample:
 
 
 
+samples = []
+for file in args.sample:
+    samples.append(Sample(os.path.basename(file), str(subprocess.check_output("md5sum " + file, shell=True))[2:34], 0))
 
-md5 = str(subprocess.check_output("md5sum " + args.sample, shell=True))[2:-3]
-md5 = md5[:32].strip()
-sample = Sample(os.path.basename(args.sample), str(subprocess.check_output("md5sum " + args.sample, shell=True))[2:34], 0)
+for sample in samples:
+    sample.strings()
+    #peFile()
+    #capa()
+    #results, malicious, undetected  = 
+    sample.virustotal()
+    #saveOutput() 
 
 
-sample.strings()
-#peFile()
-#capa()
-results, malicious, undetected  = sample.virustotal()
-#saveOutput() 
+    if args.output:
+        output_file = args.output
+    else:
+        output_file = open(sample.name + "output.html", "w") 
 
-
-if args.output:
-    output_file = args.output
-else:
-    output_file = open("output.html", "w") 
-
-output_file.write(sample.format())
+    output_file.write(sample.format())
   
-output_file.write("The detection rate is: " + str(len(malicious)) + "/" + str(len(undetected) + len(malicious)))
-output_file.close()
+    output_file.close()
 
 
 
