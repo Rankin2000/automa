0342c5bee443fd17ef587f31a46d7e678493205a
Debian <debian@debian10.linuxvmimages.local>
2021-02-09 19:36:50 +0000

    few changes

--- automa.py
+++ automa.py
@@ -17,18 +17,37 @@ args = parser.parse_args()
 malicious = False
 
 def strings():
-    os.system("python2.7 flosspractice.py " + args.sample + " >> strings.txt")
+    os.system("python2.7 flosspractice.py " + args.sample + " > strings.txt")
     f = open("strings.txt", "r")
-    print(f.read())
+    sampleStrings = f.read().splitlines()
+    f.close()
+    f = open("wordlist.txt", "r")
+    wordlist = f.read().splitlines()
+    f.close()
+    print(sampleStrings)
+    suspiciousStrings = []
+    for string in sampleStrings:
+        if string != "":
+            if string.lower() in wordlist:
+                suspiciousStrings.append(string)
 
-def saveResult():
+    print(suspiciousStrings)
+
+def saveResult(output):
     if args.output:
         output_file = args.output
     
     else:
         output_file = open("output.html", "w") 
+
+    output_file.write("Filename: " + filename)
+    output_file.write("MD5: " + md5)
     
-    output_file.write("test\n")
+
+    output_file.write("VirusTotal Results")
+    for key in results:
+        output_file.write(key + ": " + results[key]["category"])
+
     output_file.close()
 
 
@@ -42,7 +61,7 @@ def peFile():
 
     pe.print_info()
 def capa():
-    os.system("./capa " + args.sample + " >> capa.txt")
+    os.system("./capa " + args.sample + " > capa.txt")
 
 def virustotal():
     client = vt.Client(api)
@@ -53,26 +72,68 @@ def virustotal():
             analysis = client.scan_file(args.sample, wait_for_completion=True)
         file = client.get_object("/files/" + md5)
 
-    results = file.last_analysis_results
     client.close()
+    results = {}
+    malicious = []
+    undetected = []
+    for key in file.last_analysis_results:
+        results[key] = file.last_analysis_results[key]["category"]
+        if results[key] == "malicious":
+            malicious.append(key)
+        elif results[key] == "undetected":
+            undetected.append(key)
+        
+    return results, malicious, undetected
+        
+def format():
+    output = ""
+    output += "<h1>" + filename + "</h1>\n"
+    output +="<table><tr><th>Filename</th><td>" + filename + "</td></tr>"
+    output += "<tr><th>MD5</th><td>" + md5 + "</td></tr>"
+    output += "</table>"
 
-    print("VirusTotal Results")
+    output += "<h2>VirusTotal Results</h2>"
+    output += "<table>"
     for key in results:
-        print(key + ": " + results[key]["category"])
+        output += "<tr><th>" + key + "</th><td>" + results[key] + "</td></tr>"
+
+    output += "</table>"
+    return output
+
+def analysis():
     
+    #Strings
+    #VirusTotal
+    
+    
+    #if:
+    #    return true
+    #elif:
+    #    return false
+
 filename = os.path.basename(args.sample)
 
 #filesize = 0
 md5 = str(subprocess.check_output("md5sum " + filename, shell=True))[2:-3]
 md5 = md5[0:-(len(filename))].strip()
-print(filename)
-print(md5)
-
-
 
 strings()
-peFile()
+#peFile()
 capa()
-virustotal()
-saveResult()
- 
+results, malicious, undetected  = virustotal()
+#saveOutput() 
+
+
+if args.output:
+    output_file = args.output
+else:
+    output_file = open("output.html", "w") 
+
+output_file.write(format())
+  
+output_file.write("The detection rate is: " + str(len(malicious)) + "/" + str(len(undetected) + len(malicious)))
+output_file.close()
+
+
+
+
