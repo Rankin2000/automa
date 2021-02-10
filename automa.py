c6875b66063ebc65fa48c734c1d5b036fa4f4017
Debian <debian@debian10.linuxvmimages.local>
2021-02-10 04:13:08 +0000

    added analysis and explanation to output

--- automa.py
+++ automa.py
@@ -17,29 +17,32 @@ args = parser.parse_args()
 
 
 class Sample:
-    def __init__(self, name, md5, size):
+    def __init__(self, name):
         self.name = name
-        self.md5 = md5
-        self.size = size 
+        self.md5 = str(subprocess.check_output("md5sum " + name, shell=True))[2:34]
+        self.size = os.path.getsize(name) 
 
-    malicious = False
+    malware = False
 
     def strings(self):
         os.system("python2.7 flosspractice.py " + self.name + " > strings.txt")
+
         f = open("strings.txt", "r")
         sampleStrings = f.read().splitlines()
         f.close()
+        
         f = open("wordlist.txt", "r")
         wordlist = f.read().splitlines()
         f.close()
-        print(sampleStrings)
-        suspiciousStrings = []
+        
+        #print(sampleStrings)
+        self.suspiciousStrings = []
         for string in sampleStrings:
             if string != "":
                 if string.lower() in wordlist:
-                    suspiciousStrings.append(string)
+                    self.suspiciousStrings.append(string)
 
-        print(suspiciousStrings)
+        #print(self.suspiciousStrings)
 
     
     def peFile(self):
@@ -74,8 +77,6 @@ class Sample:
             elif self.results[key] == "undetected":
                 self.undetected.append(key)
         
-       # results, malicious, undetected
-        
     def format(self):
         output = ""
         output += "<h1>" + self.name + "</h1>\n"
@@ -83,41 +84,52 @@ class Sample:
         output += "<tr><th>MD5</th><td>" + self.md5 + "</td></tr>"
         output += "</table>"
 
+        if self.malware:
+            output += "<p>The sample is believed to be malicious</p>"
+            output += "<h5>Reasons:</h5>"
+            output += "<table>"
+            for reason in self.reasons:
+                output += "<tr><td>" + reason + "</td><td>"
+                output += "<ul>"
+
+                for evidence in self.reasons[reason]:
+                    output += "<li>" + evidence + "</li>"
+                output += "</ul></td></tr>"
+            output += "</table>"
+            
         output += "<h2>VirusTotal Results</h2>"
         output += "<table>"
         for key in self.results:
             output += "<tr><th>" + key + "</th><td>" + self.results[key] + "</td></tr>"
 
-
         output += "</table>"
         output += "The detection rate is: " + str(len(self.malicious)) + "/" + str(len(self.undetected) + len(self.malicious))
         return output
 
-#def analysis():
-    
-    #Strings
-    #VirusTotal
-    
-    
-    #if:
-    #    return true
-    #elif:
-    #    return false
+    def analysis(self):
+        self.reasons = {}
 
-
-
+        if self.malicious:
+            self.malware = True
+            self.reasons["VirusTotal"] = self.malicious
+        if self.suspiciousStrings:
+            self.malware = True
+            self.reasons["FLOSS"] = self.suspiciousStrings
+ 
+#Creates list based on files passed
 samples = []
 for file in args.sample:
-    samples.append(Sample(os.path.basename(file), str(subprocess.check_output("md5sum " + file, shell=True))[2:34], 0))
+    samples.append(Sample(file)) 
 
+#Scan each file that was passed
 for sample in samples:
     sample.strings()
     #peFile()
     #capa()
-    #results, malicious, undetected  = 
     sample.virustotal()
     #saveOutput() 
 
+    sample.analysis()
 
     if args.output:
         output_file = args.output
