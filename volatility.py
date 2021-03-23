b0675c2f8a3785c7e00480e05a0717ca23176a34
Debian <debian@debian10.linuxvmimages.local>
2021-03-23 03:28:35 +0000

    small fixes

--- volatility.py
+++ volatility.py
@@ -19,21 +19,43 @@ def plugin(plugin):
             data = json.load(volatility)
         return data 
 
+
+def getrules(directory):
+    dirlist = os.listdir(directory)
+    files = []
+    for item in dirlist:
+        path = os.path.join(directory, item)
+        if os.path.isdir(path):
+            files = files + getrules(path)
+        else:
+            files.append(path)
+    return files
+
+
+def yarascan(rules):
+    if os.path.isfile(rules):
+        #Run as file
+        os.system("/home/debian/Desktop/honours/volatility2.6_standalone --profile='Win7SP1x64' -f dump.elf yarascan -y " + rules)
+    else:
+        for item in getrules(rules):
+            os.system("/home/debian/Desktop/honours/volatility2.6_standalone --profile='Win7SP1x64' -f dump.elf yarascan -y " + item)
+
 if __name__ == "__main__":
 
-    getdump()
-    pid = 2592
-    data = plugin("ramscan")
-    print(data["columns"])
-    for item in data["rows"]:
-        if item[1] == pid:
-            print("Sample is here" + item[0])
+    yarascan("/home/debian/Desktop/honours/rules/rules/malware")
+    #getdump()
+    #pid = 2592
+    #data = plugin("ramscan")
+    #print(data["columns"])
+    #for item in data["rows"]:
+    #    if item[1] == pid:
+    #        print("Sample is here" + item[0])
 
-        elif item[-1]:
-            print(item[0] + " has a " + item[-1])
+    #    elif item[-1]:
+   #         print(item[0] + " has a " + item[-1])
 
 
-    data = plugin("cmdcheck")
-    print(data["columns"])
-    for item in data["rows"]:
-        print(item)
+   # data = plugin("cmdcheck")
+   # print(data["columns"])
+   # for item in data["rows"]:
+   #     print(item)
