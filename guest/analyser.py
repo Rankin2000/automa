b2ff71d7878b039f2bff831f3898342fe55d0516
Debian <debian@debian10.linuxvmimages.local>
2021-03-25 20:20:01 +0000

    added pesieve and changed guest functionality

--- guest/analyser.py
+++ guest/analyser.py
@@ -1,6 +1,7 @@
 import time
 import subprocess, os
 
+
 def procmon():
     os.system("C:\\Users\IEUser\Downloads\ProcessMonitor\ProcMon.exe -accepteula /backingfile C:\\Users\IEUser\Desktop\log.pml /quiet")
     time.sleep(5)
@@ -8,8 +9,24 @@ def procmon():
 
 def run(filename):
     subprocess.run(["C:\\Users\IEUser\Desktop\\" + filename])
+    tasklist = subprocess.check_output(["tasklist", "/FO", "csv"])
+    tasklist = tasklist.decode().split('\n')
+    for item in tasklist:
+        item = item.replace("\"", "")
+        item = item.split(',')
+        if item[0] == "cmd.exe":
+            print(item[1])
+            return item[1]
 
+
+def pesieve(pid):
+    os.system("C:\\Users\IEUser\Downloads\pe-sieve64.exe /pid " + pid + " /quiet /json > pe-sieve.json")
+    
 def analyse(filename):
-    run(filename)
-    procmon()
+    #procmon()
+    pid = run(filename)
+    pesieve(pid)
     return filename
+
+if __name__ == "__main__":
+    analyse("helloworld.exe")
--- guest/sockets.py
+++ guest/sockets.py
@@ -1,46 +1,71 @@
 import socket
 import analyser
-HOST = '192.168.164.138' #Host IP
+import time
+HOST = '192.168.56.1' #Host IP
 PORT = 8080
 BUFFER_SIZE = 4096
 
 
+def send(file):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #while True:
+        #    try:
+        #        s.connect((HOST, PORT))
+        #        break
+        #    except:
+        #        pass
+        #s.sendall(data.encode())
 
-    
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('', PORT))
-    s.listen()
-    conn, addr = s.accept()
-
-    with conn:
-        print('Connected by', addr)
-
-        filename = conn.recv(BUFFER_SIZE).decode('utf-8')
-            
-        with open(filename, "wb") as f:
+        s.connect((HOST, PORT))
+        with open(file, "rb") as f:
             while True:
-                data = conn.recv(BUFFER_SIZE)
+                data = f.read(BUFFER_SIZE)
                 if not data:
-                    #analyse
-                    
-                    print("analysing..")
                     break
+                s.sendall(data)
+    return True
 
-                f.write(data)
-                print("writing...")
-                
-        
+def receive():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', PORT))
+        s.listen()
+        conn, addr = s.accept()
 
-        print("finished")
-        
-        
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    while True:
-        try:
-            s.connect((HOST, PORT))
-            break
-        except:
-            pass
+        with conn:
+            print('Connected by', addr)
 
+            filename = conn.recv(BUFFER_SIZE).decode()
+            conn.sendall(filename.encode())
+            
+            with open(filename, "wb") as f:
+                while True:
+                    
+                    data = conn.recv(BUFFER_SIZE)
+        
+                    if not data:
+                        #analyse
+                        break
+                        print("analysing..")
+                        
+                    f.write(data)
+                    #conn.sendall("Received".encode())
+                    print("writing...")
+
+            
+
+            
+            return filename
     
-    s.send(analyser.analyse(filename).encode())
+            
+
+while True:
+    filename = False
+    while not filename:
+        try:
+            filename = receive()
+        except UnicodeDecodeError:
+            print("Odd Unicode bug, Trying again")
+        except:
+            raise
+    analyser.analyse(filename)
+    send("pe-sieve.json")
