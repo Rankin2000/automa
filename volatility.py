180fa41b3faf9563673f82c202548b758ddd750c
Stuart <stuart@debian.honours.com>
2021-04-20 17:56:05 +0100

    changed directory for new environment, needs improved

--- volatility.py
+++ volatility.py
@@ -9,13 +9,15 @@ def getdump():
 
 def plugin(plugin):
     #Remove previous json files as plugin doesn't like overwriting
-    os.system("rm /home/debian/Desktop/honours/volatility/" + plugin + ".json")
+    os.system("rm /home/stuart/Desktop/honours/volatility/" + plugin + ".json")
     #Run plugin
-    os.system("/home/debian/Desktop/honours/volatility2.6_standalone --plugins='/home/debian/volatility-plugins/" + plugin + "' --profile='Win7SP1x64' -f /home/debian/Desktop/honours/dump.elf " + plugin + " --output=json --output-file=/home/debian/Desktop/honours/volatility/" + plugin + ".json")
+#    os.system("/home/stuart/Desktop/honours/volatility2.6_standalone --plugins='/home/stuart/volatility-plugins/" + plugin + "' --profile='Win7SP1x64' -f /home/stuart/Desktop/honours/dump.elf " + plugin + " --output=json --output-file=/home/stuart/Desktop/honours/volatility/" + plugin + ".json")
+    os.system("volatility --plugins='/home/stuart/Desktop/honours/volatility/' --profile='Win7SP1x64' -f /home/stuart/Desktop/honours/dump.elf " + plugin + " --output=json --output-file=/home/stuart/Desktop/honours/volatility/" + plugin + ".json")
 
 
-    if os.path.isfile("/home/debian/Desktop/honours/volatility/" + plugin + ".json"):
-        with open("/home/debian/Desktop/honours/volatility/" + plugin + ".json") as volatility:
+
+    if os.path.isfile("/home/stuart/Desktop/honours/volatility/" + plugin + ".json"):
+        with open("/home/stuart/Desktop/honours/volatility/" + plugin + ".json") as volatility:
             data = json.load(volatility)
         return data 
 
@@ -35,14 +37,15 @@ def getrules(directory):
 def yarascan(rules):
     if os.path.isfile(rules):
         #Run as file
-        os.system("/home/debian/Desktop/honours/volatility2.6_standalone --profile='Win7SP1x64' -f dump.elf yarascan -y " + rules)
+        #os.system("/home/stuart/Desktop/honours/volatility2.6_standalone --profile='Win7SP1x64' -f dump.elf yarascan -y " + rules)
+        os.system("volatility --profile='Win7SP1x64' -f dump.elf yarascan -y " + rules)
     else:
         for item in getrules(rules):
-            os.system("/home/debian/Desktop/honours/volatility2.6_standalone --profile='Win7SP1x64' -f dump.elf yarascan -y " + item)
+            os.system("volatility --profile='Win7SP1x64' -f dump.elf yarascan -y " + item)
 
 if __name__ == "__main__":
 
-    yarascan("/home/debian/Desktop/honours/rules/rules/malware")
+#    yarascan("/home/stuart/Desktop/honours/rules/rules/malware")
     #getdump()
     #pid = 2592
     #data = plugin("ramscan")
@@ -55,7 +58,7 @@ if __name__ == "__main__":
    #         print(item[0] + " has a " + item[-1])
 
 
-   # data = plugin("cmdcheck")
-   # print(data["columns"])
-   # for item in data["rows"]:
-   #     print(item)
+    data = plugin("cmdcheck")
+    print(data["columns"])
+    for item in data["rows"]:
+        print(item)
